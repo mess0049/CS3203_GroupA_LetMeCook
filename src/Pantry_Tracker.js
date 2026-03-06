6c6f8b0e77754abc8f603dce2be98de5822f2ac5
s_hyun <Seonghyun.Park-1@ou.edu>
2026-03-06 17:17:23 -0600

    changed function in js

--- src/Pantry_Tracker.js
+++ src/Pantry_Tracker.js
@@ -1,31 +1,38 @@
 //dummy database need to change to database
-const myIngredients = [
+const useringredient = [
     { name: "Egg", quantity: 5},
     { name: "Milk", quantity: 1},
     { name: "Tomato", quantity: 3},
     { name: "Bread", quantity: 2}
 ];
 
-const recipeDatabase = [
+const ingredients_in_refrigeator = [
     { name: "Fried Egg", ingredients: ["Egg"] },
     { name: "Tomato Omelette", ingredients: ["Egg", "Tomato"] },
     { name: "French Toast", ingredients: ["Egg", "Milk", "Bread"] },
     { name: "Milkshake", ingredients: ["Milk", "Ice Cream"] }
 ];
 //dummy database need to change to database
-
+ 
 //show refrigeator_ingredients when load page
 function displayPantry() {
     const pantryBody = document.getElementById("pantryBody");
     pantryBody.innerHTML = ""; //Reset
 
-    myIngredients.forEach(item => {
-        const row = `<tr>
-            <td>${item.name}</td>
-            <td>${item.quantity}</td>
-        </tr>`;
+    useringredient.forEach(function(pantry)
+        {
+        const row = '<tr>' + 
+                '<td>' + pantry.name + '</td>' + 
+                '<td>' + pantry.quantity + '</td>' + 
+              '</tr>';
         pantryBody.innerHTML += row;
-    });
+        }
+    );
+}
+
+
+function recipe_check() {
+//fill this
 }
 
 //when press recommend button
@@ -34,20 +41,24 @@ function recommend() {
     recipeListElement.innerHTML = ""; //reset
 
     //ingredient name
-    const myInvenNames = myIngredients.map(item => item.name);
+    const user_ingredient_names = [];
+    for (let i = 0; i < useringredient.length; i++) {
+        user_ingredient_names.push(useringredient[i].name);
+    }
 
+    
     //find recipe(check to exist all ingredient of recipe)
-    const availableRecipes = recipeDatabase.filter(recipe => {
-        return recipe.ingredients.every(needed => myInvenNames.includes(needed));
-    });
+    const final_recipes = recipe_check();
+
 
     //show in window
-    if (availableRecipes.length > 0) {
-        availableRecipes.forEach(recipe => {
-            const li = document.createElement("li");
-            li.textContent = `${recipe.name} (Ingredients: ${recipe.ingredients.join(", ")})`;
+    if (final_recipes.length > 0) {
+        for (let i = 0; i < availableRecipes.length; i++) {
+            let recipe = availableRecipes[i];
+            let li = document.createElement("li");
+            li.textContent = recipe.name + " (Ingredients: " + recipe.ingredients.join(", ") + ")";
             recipeListElement.appendChild(li);
-        });
+        }
     } else {
         recipeListElement.innerHTML = "<li>No recipes found. Try adding more ingredients!</li>";
     }
